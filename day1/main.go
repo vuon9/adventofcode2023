e955ce8e5d1fab72a2daea00af69c1d668edc003
Vuong <3168632+vuon9@users.noreply.github.com>
2023-12-01 06:52:46 +0000

    wip: part 2 day 1

--- day1/main.go
+++ day1/main.go
@@ -1010,7 +1010,84 @@ eightrtsjszc2
 `
 
 func main() {
-	fmt.Println("Part 1, result is", part1(input))
+	fmt.Println("Part 1, result is: ", part1(input))
+	fmt.Println("Part 2, result is: ", part2(input))
+}
+
+func part2(input string) string {
+	lines := strings.Split(input, "\n")
+	strToNum := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
+	}
+
+	total := 0
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+
+		fn, ln := "", ""
+		for i, j := 0, len(line)-1; i < len(line); i, j = i+1, j-1 {
+			if fn == "" {
+				foundFn := false
+				for str, num := range strToNum {
+					if len(line) >= i+len(str)-1 {
+						qw := line[i : i+len(str)-1]
+						if str == qw {
+							fn = num
+							i += len(str) - 1
+							foundFn = true
+							break
+						}
+					}
+				}
+
+				if !foundFn {
+					ni, err := strconv.Atoi(string(line[i]))
+					if err == nil {
+						fn = fmt.Sprintf("%d", ni)
+					}
+				}
+			}
+
+			if ln == "" {
+				foundLn := false
+
+				for str, num := range strToNum {
+					if j-len(str)-1 > 0 {
+						qw := line[j-len(str)-1 : j]
+						if str == qw {
+							ln = num
+							foundLn = true
+							j -= len(str) - 1
+							break
+						}
+					}
+				}
+
+				if !foundLn {
+					nj, err := strconv.Atoi(string(line[j]))
+					if err == nil {
+						ln = fmt.Sprintf("%d", nj)
+					}
+				}
+			}
+		}
+
+		fnn, _ := strconv.Atoi(fmt.Sprintf("%s%s", fn, ln))
+		// fmt.Println("line: ", lineNo, ", number is: ", fnn)
+		total += fnn
+	}
+
+	return fmt.Sprintf("%d", total)
 }
 
 func part1(input string) string {
