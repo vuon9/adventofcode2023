c2046d43a5d8fbc13e0f2185d0ac37ddde80845f
Vuong <3168632+vuon9@users.noreply.github.com>
2023-12-02 11:35:57 +0000

    Refactor number conversion logic in part2 function

--- day1/main.go
+++ day1/main.go
@@ -9,77 +9,81 @@ import (
 	"github.com/vuon9/adventofcode2023/utils"
 )
 
+var strToNum = map[string]int{
+	"one":   1,
+	"two":   2,
+	"three": 3,
+	"four":  4,
+	"five":  5,
+	"six":   6,
+	"seven": 7,
+	"eight": 8,
+	"nine":  9,
+}
+
+var charToNum = map[string]int{
+	"1": 1,
+	"2": 2,
+	"3": 3,
+	"4": 4,
+	"5": 5,
+	"6": 6,
+	"7": 7,
+	"8": 8,
+	"9": 9,
+}
+
 func main() {
 	input := utils.ReadFileOrPanic()
+	// 54634
 	fmt.Println("Part 1, result is: ", part1(string(input)))
+	// 53855
 	fmt.Println("Part 2, result is: ", part2(string(input)))
 }
 
 func part2(input string) string {
 	lines := strings.Split(input, "\n")
-	strToNum := map[string]string{
-		"one":   "1",
-		"two":   "2",
-		"three": "3",
-		"four":  "4",
-		"five":  "5",
-		"six":   "6",
-		"seven": "7",
-		"eight": "8",
-		"nine":  "9",
-	}
 
 	total := 0
 	for _, line := range lines {
-		fn, ln := "", ""
+		fn, ln := 0, 0
 		for i, j := 0, len(line)-1; i < len(line); i, j = i+1, j-1 {
-			if fn == "" {
-				foundFn := false
+			if fn == 0 {
 				for str, num := range strToNum {
 					if len(line) >= i+len(str) {
 						qw := line[i : i+len(str)]
 						if str == qw {
 							fn = num
 							i += len(str)
-							foundFn = true
 							break
 						}
 					}
 				}
 
-				if !foundFn {
-					ni, err := strconv.Atoi(string(line[i]))
-					if err == nil {
-						fn = fmt.Sprintf("%d", ni)
-					}
+				if fn == 0 {
+					fn, _ = charToNum[string(line[i])]
 				}
 			}
 
-			if ln == "" {
-				foundLn := false
-
+			if ln == 0 {
 				for str, num := range strToNum {
 					if j-len(str) >= 0 {
 						qw := line[j-len(str)+1 : j+1]
 						if str == qw {
 							ln = num
-							foundLn = true
 							j -= len(str)
 							break
 						}
 					}
 				}
 
-				if !foundLn {
-					nj, err := strconv.Atoi(string(line[j]))
-					if err == nil {
-						ln = fmt.Sprintf("%d", nj)
-					}
+				if ln == 0 {
+					ln = charToNum[string(line[j])]
 				}
 			}
 		}
 
-		fnn, _ := strconv.Atoi(fmt.Sprintf("%s%s", fn, ln))
+		fnn, _ := strconv.Atoi(fmt.Sprintf("%d%d", fn, ln))
 		total += fnn
 	}
 
@@ -113,7 +117,6 @@ func part1(input string) string {
 		}
 
 		fnn, _ := strconv.Atoi(fmt.Sprintf("%s%s", fn, ln))
-		// fmt.Println("line: ", lineNo, ", number is: ", fnn)
 		total += fnn
 	}
 
