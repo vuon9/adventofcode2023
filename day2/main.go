e8c6af8bb5357fc323826b9d46a2b8f55db4e9aa
Vuong <3168632+vuon9@users.noreply.github.com>
2023-12-02 13:46:12 +0000

    Refactor part2 function to calculate total color value

--- day2/main.go
+++ day2/main.go
@@ -22,7 +22,10 @@ type turn struct {
 
 func main() {
 	input := utils.ReadFileOrPanic()
+	// 2551
 	fmt.Println("Part 1, result is: ", part1(input, expectation{Red: 12, Green: 13, Blue: 14}))
+	// 62811
+	fmt.Println("Part 2, result is: ", part2(input))
 }
 
 func part1(input string, exp expectation) string {
@@ -71,5 +74,39 @@ func part1(input string, exp expectation) string {
 
 func part2(input string) string {
 	total := 0
+
+	for _, line := range strings.Split(input, "\n") {
+		if line == "" {
+			continue
+		}
+
+		mainStr := strings.Split(line, ": ")
+		rawRounds := strings.Split(mainStr[1], "; ")
+
+		mRed, mGreen, mBlue := 0, 0, 0
+		for _, rawRound := range rawRounds {
+			rawTurns := strings.Split(rawRound, ", ")
+			red, green, blue := 0, 0, 0
+			for _, rawTurn := range rawTurns {
+				bag := strings.Split(strings.TrimSpace(rawTurn), " ")
+				noBag, _ := strconv.Atoi(bag[0])
+
+				if bag[1] == "red" {
+					red = noBag
+				} else if bag[1] == "green" {
+					green = noBag
+				} else if bag[1] == "blue" {
+					blue = noBag
+				}
+			}
+
+			mRed = max(mRed, red)
+			mGreen = max(mGreen, green)
+			mBlue = max(mBlue, blue)
+		}
+
+		total += mRed * mGreen * mBlue
+	}
+
 	return fmt.Sprintf("%d", total)
 }
