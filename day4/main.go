1e16ab2445d32c187fd882708ec2eb24c112ec8c
Vuong <3168632+vuon9@users.noreply.github.com>
2023-12-04 16:26:36 +0000

    Add day 4 p1 solution

---
+++ day4/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/vuon9/adventofcode2023/utils"
+)
+
+func main() {
+	input := utils.ReadFileOrPanic()
+	// 23028
+	fmt.Println("Part 1: ", part1(input))
+	//
+	// fmt.Println("Part 2: ", part2(input))
+}
+
+func part1(input string) int {
+	total := 0
+
+	for _, rawCard := range strings.Split(input, "\n") {
+		cardName := ""
+		winNumbers := make(map[int]struct{})
+		myCardNumbers := make([]int, 0)
+		currentNumber := ""
+
+		winNumbersStart := 0
+		myNumbersStart := 0
+
+		for i := 0; i < len(rawCard); i++ {
+			if rawCard[i] == ':' {
+				cardName = rawCard[:i]
+				winNumbersStart = i + 2
+			}
+
+			if rawCard[i] == '|' {
+				myNumbersStart = i + 2
+			}
+
+			if winNumbersStart > 0 && myNumbersStart == 0 {
+				if rawCard[i] >= 48 && rawCard[i] <= 57 {
+					currentNumber += string(rawCard[i])
+				} else if currentNumber != "" {
+					num, _ := strconv.Atoi(currentNumber)
+					winNumbers[num] = struct{}{}
+					currentNumber = ""
+				}
+			}
+
+			if myNumbersStart > 0 {
+				isANumber := false
+				if rawCard[i] >= 48 && rawCard[i] <= 57 {
+					currentNumber += string(rawCard[i])
+					isANumber = true
+				}
+
+				if (!isANumber && currentNumber != "") || i == len(rawCard)-1 {
+					num, _ := strconv.Atoi(currentNumber)
+					myCardNumbers = append(myCardNumbers, num)
+					currentNumber = ""
+				}
+			}
+		}
+
+		wonCardNumbers := make([]int, 0)
+		for _, num := range myCardNumbers {
+			_, found := winNumbers[num]
+			if found {
+				wonCardNumbers = append(wonCardNumbers, num)
+			}
+		}
+
+		cardTotal := 0
+		for i := 0; i < len(wonCardNumbers); i++ {
+			if i == 0 {
+				cardTotal += 1
+			} else {
+				cardTotal *= 2
+			}
+		}
+
+		total += cardTotal
+
+		fmt.Println(cardName, ":\n--", winNumbers, "\n--", myCardNumbers, wonCardNumbers, "=>", cardTotal)
+	}
+
+	return total
+}
+
+func part2(input string) int {
+	return 0
+}
